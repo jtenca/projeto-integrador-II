cdbcb026c196a92e94f3ddfebbad0b3e274b62bc
Erwin Bogner <erwin.bogner@fatec.sp.gov.br>
2019-11-22 21:15:52 -0300

    atualização index expressionismo outros

--- includes/head.php
+++ includes/head.php
@@ -7,6 +7,9 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/bootstrap-grid.min.css">
     <link rel="stylesheet" href="css/estilo.css" />
+    <link rel="stylesheet" href="css/xfade2.css" />
+    <link rel="stylesheet" type="text/css" href="css/xfade2_o.css" />
+    <script type="text/javascript" src="js/xfade2.js"></script>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Advent+Pro&display=swap" rel="stylesheet">
    <!-- <link href="https://fonts.googleapis.com/css?family=Antic+Slab&display=swap" rel="stylesheet">-->
--- index.php
+++ index.php
@@ -104,9 +104,17 @@
                                     <a href="https://www1.folha.uol.com.br/folhinha/2019/11/no-dia-da-bandeira-conheca-cinco-curiosidades-sobre-o-simbolo-nacional.shtml"><strong>Saiba mais</strong></a>
                                 </p>
                             </div>
-                            <div class="zoom">
-                                <img src="img/ind11_BandeiraBrasil.jpg" alt="Bandeira do Brasil" width="350">
+                            <div id="imageContainer">
+                                <img src="img/ind_Quadro_bandeiraBrasil_01.jpg"/>
+                                <img src="img/ind_Quadro_bandeiraBrasil_02.jpg"/>
+                                <img src="img/ind_Quadro_bandeiraBrasil_03.jpg"/>
+                                <img src="img/ind_Quadro_bandeiraBrasil_04.jpg"/>
+                                <img src="img/ind_Quadro_bandeiraBrasil_05.jpg"/>
+                                <img src="img/ind11_BandeiraBrasil.jpg"/>
                             </div>
+                            <!--<div class="zoom">
+                                <img src="img/ind11_BandeiraBrasil.jpg" alt="Bandeira do Brasil" width="350">
+                            </div>!-->
                         </div>
                     </div>
                     <div class="row">
@@ -261,6 +269,7 @@
         </div>
     </div>
 
+    <script src="js/xfade2.js" type='text/javascript'/>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
