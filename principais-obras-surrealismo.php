e8b94c47f69da31ab4bc29f60cfca4a68702aa02
macenadev <kelvin_pqp@hotmail.com>
2019-11-25 19:06:54 -0300

    adicionando imagens

--- principais-obras-surrealismo.php
+++ principais-obras-surrealismo.php
@@ -3,7 +3,7 @@
 
 <head>
     <?php include('includes/head.php'); ?>
-    <title>Principais artistas surrealistas</title>
+    <title>Principais obras surrealistas</title>
 </head>
 
 <body>
@@ -29,71 +29,71 @@
         <!---------------- CONTEÚDO --------------->
 
         <div class="container">
-        <div class="row">
-            <div class="col-12">
+            <div class="row">
+                <div class="col-12">
 
-                <h5>Principais Artistas</h5>
+                    <h5>Obras Surrealistas</h5>
 
-            </div>
-            <div class="container">
-                <div class="text-center">
-                    <div class="row">
-                        <div class="col-6 ">
-                        <img src="img/salvador-dali.jpg" />
-                            <p class="artistas">Salvador Dali</p>
+                </div>
+                <div class="container">
+                    <div class="text-center">
+                        <div class="row">
+                            <div class="col-6 ">
+                                <img src="img/a-persistencia-da-memoria-1931.jpg" />
+                                <p class="artistas">A persistência da Memória - Salvador Dalli</p>
+                            </div>
+
+
+                            <div class="col-6 text-center">
+                                <img src="img/a-roda-da-luz-1925.jpg" />
+                                <p class="artistas">A roda da Luz - Max Ernst</p>
+
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">
+                                <img src="img/a-traicao-das-imagens-1929.jpg" />
+                                <p class="artistas"> A traição das imagens - René Magritte</p>
+
+                            </div>
+
+                            <div class="col-6">
+                                <img src="img/carnaval-de-arlequim-1924-25.jpg" />
+                                <p class="artistas">Carnaval de alerquim - Joan Miró</p>
+
+
+                            </div>
                         </div>
 
+                        <div class="row">
+                            <div class="col-6">
+                                <img src="img/paul-delvaux.jpg" />
+                                <p class="artistas">Paul Delvaux</p>
 
-                        <div class="col-6 text-center">
-                            <img src="img/joan-miro.jpg" />
-                            <p class="artistas">Joan Miro</p>
-
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-6">
-                            <img src="img/magritte.jpg" />
-                            <p class="artistas">René Magritte</p>
-
-                        </div>
-
-                        <div class="col-6">
-                            <img src="img/Remedios-Varo.jpg" />
-                            <p class="artistas">Varo Remédios</p>
+                            </div>
+                            <div class="col-6">
+                                <img src="img/max-ernst.jpg" />
+                                <p class="artistas">Max Ernest</p>
 
 
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-6">
-                            <img src="img/paul-delvaux.jpg" />
-                            <p class="artistas">Paul Delvaux</p>
-
-                        </div>
-                        <div class="col-6">
-                            <img src="img/max-ernst.jpg" />
-                            <p class="artistas">Max Ernest</p>
-
-
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <!-------------------------------->
-        <div class="row">
-            <div class="col-12 fixed-bottom">
-                <?php include('includes/footer.php'); ?>
+            <!-------------------------------->
+            <div class="row">
+                <div class="col-12 fixed-bottom">
+                    <?php include('includes/footer.php'); ?>
+                </div>
             </div>
+
         </div>
 
-    </div>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 
 </html>
