12dd806ff13c15173777b3cde49fe620454b5729
Erwin Bogner <erwin.bogner@fatec.sp.gov.br>
2019-11-26 10:15:34 -0300

    compressão imagens

--- includes/footer.php
+++ includes/footer.php
@@ -4,22 +4,15 @@
         <a class="text-left" href="#">Quem Somos</a>
         <a href="#">Fale Conosco</a>
         </div>!-->
-        
         <div class="icones col-6">
             <img class="ml-lg-5" src="img/fb_icon.png" alt="ícone do facebook" />
             <img src="img/instagram_icon.png" alt="ícone do facebook" />
             <img src="img/twitter_icon.png" alt="ícone do facebook" />
         </div>
-
-  
         <div class="col-6 float-right">
-        
             <li class=""><a href="quem-somos.php">Quem somos</a></li>
             <li class=""><a href="faleconosco01.php">Fale conosco</a></li>
             <li class=""><a href="normas.php">Normas utilização</a></li>
-
         </div>
- 
     </section>
-
 </footer>
--- index.php
+++ index.php
@@ -281,7 +281,7 @@
                 <hr noshade="noshade" width=“100%” size=“100”>
             </div>
         </article>
-    </div> <!-- fechamento div container            !-->
+    </div> <!-- fechamento div container           !-->
 
     <!--------------------------------footer       !-->
     <div class="row">
@@ -289,7 +289,6 @@
             <?php include('includes/footer.php'); ?>
         </div>
     </div>
-
     <script src="js/xfade2.js" type='text/javascript' />
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
